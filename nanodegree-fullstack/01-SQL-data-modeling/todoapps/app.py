5b542fdf57fbfe44d5b6aae491a3b01ed41fd7e1
Andika Nugraha <m.andika.nugraha@gmail.com>
2021-05-03 00:46:14 +0700

    Ajax request

--- nanodegree-fullstack/01-SQL-data-modeling/todoapps/app.py
+++ nanodegree-fullstack/01-SQL-data-modeling/todoapps/app.py
@@ -1,5 +1,7 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, jsonify, abort
 from flask_sqlalchemy import SQLAlchemy
+import json
+import sys
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:Pass2021!@database:5432/udacity_fullstack_02'
@@ -39,11 +41,25 @@ def index():
 
 @app.route('/todos/create', methods=['POST'])
 def create_todo():
-    description = request.form.get('description', '')
-    todo = Todo(description=description)
-    db.session.add(todo)
-    db.session.commit()
-    return redirect(url_for('index'))
+    # description = request.form.get('description', '')
+    error = False
+    body = {}
+    try:
+        description = request.get_json()['description']
+        todo = Todo(description=description)
+        db.session.add(todo)
+        db.session.commit()
+        body['description'] = todo.description
+    except:
+        db.session.rollback()
+        print(sys.exc_info())
+        error = True
+    finally:
+        db.session.close()
+    if error:
+        abort(400)
+    else:
+        return jsonify(body)
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0")
