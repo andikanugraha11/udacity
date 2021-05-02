ebc724d0b2d91d3050010d9c8137e4c7f98f7bde
Andika Nugraha <m.andika.nugraha@gmail.com>
2021-05-02 23:44:31 +0700

    View, models, controller

--- nanodegree-fullstack/01-SQL-data-modeling/todoapps/app.py
+++ nanodegree-fullstack/01-SQL-data-modeling/todoapps/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -7,24 +7,43 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
+class Todo(db.Model):
+    __tablename__ = 'todos'
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String(), nullable=False)
+
+    def __repr__(self):
+        return f'<Todo ID: {self.id}, description: {self.description}>'
+
+db.create_all()
+
 @app.route('/')
 def index():
-    data = [
-        {
-        'description': 'Todo 1'
-        },
-        {
-        'description': 'Todo 2'
-        },
-        {
-        'description': 'Todo 3'
-        },
-        {
-        'description': 'Todo 4'
-        }
-    ]
+    # data = [
+    #     {
+    #     'description': 'Todo 1'
+    #     },
+    #     {
+    #     'description': 'Todo 2'
+    #     },
+    #     {
+    #     'description': 'Todo 3'
+    #     },
+    #     {
+    #     'description': 'Todo 4'
+    #     }
+    # ]
+
+    data = Todo.query.all()
     return render_template('index.html', data=data)
 
+@app.route('/todos/create', methods=['POST'])
+def create_todo():
+    description = request.form.get('description', '')
+    todo = Todo(description=description)
+    db.session.add(todo)
+    db.session.commit()
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0")
